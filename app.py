707926bb93f7a40c0fc15fb274ce5a0a79f08e01
Fada Mannie <opokuakwasi239@gmail.com>
2026-03-16 14:54:44 -0500

    check_guess

--- app.py
+++ app.py
@@ -36,16 +36,16 @@ def check_guess(guess, secret):
 
     try:
         if guess > secret:
-            return "Too High", "📈 Go HIGHER!"
+            return "Too High", "GO LOWER!"
         else:
-            return "Too Low", "📉 Go LOWER!"
+            return "Too Low", "GO HIGHER!"
     except TypeError:
         g = str(guess)
         if g == secret:
             return "Win", "🎉 Correct!"
         if g > secret:
-            return "Too High", "📈 Go HIGHER!"
-        return "Too Low", "📉 Go LOWER!"
+            return "Too High", " Go LOWER!"
+        return "Too Low", "📈 Go HIGHER!"
 
 
 def update_score(current_score: int, outcome: str, attempt_number: int):
